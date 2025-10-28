75125e183952a3e479007f29dc668303a1e32643
Vinicius Damasceno <38514244+vdamasceno@users.noreply.github.com>
2025-10-28 09:41:53 -0300

    Corrige bug fatal no path da rota app.options

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 // Rota de teste
 app.get('/', (req, res) => {
